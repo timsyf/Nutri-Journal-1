285ebe9e7a4bf71bc524d99bd65c7ba40157d090
haruthepotato <timothysoonyufeng@gmail.com>
2023-06-09 11:37:41 +0800

    Vite and React Router Set up

---
+++ Nutri-Journal/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path="">
+          <Route path="" />
+        </Route>
+        <Route path="" />
+      </Routes>
+    </>
+  )
+}
+
+export default App
