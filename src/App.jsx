ad1a0ab5e621a1fe6a3050786568215de1ba26fc
haruthepotato <timothysoonyufeng@gmail.com>
2023-06-09 13:39:34 +0800

    Test fetch on vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
+  const [result, setResult] = useState({});
+  
+  const handleSearch = async () => {
+    console.log("searching");
+    const response = await fetch(
+      `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=9MD6Im68ci8QJf3fHSBycrAbvkNNFKGcnr2bMtJ2&query=Cheddar%20Cheese`
+    );
+    const jsonData = await response.json();
+    setResult(jsonData);
+    console.log(jsonData);
+  };
 
   return (
     <>
-      <Routes>
-        <Route path="">
-          <Route path="" />
-        </Route>
-        <Route path="" />
-      </Routes>
+      <button onClick={handleSearch}>Search</button>
     </>
   )
 }
